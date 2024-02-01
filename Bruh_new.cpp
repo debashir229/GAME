efd9ca4699b5e20cd38771f7b142a0cd878d682b
N <121445609+debashir229@users.noreply.github.com>
2024-02-01 19:18:06 +0400

    Bruh_new.cpp

--- Bruh_new.cpp
+++ Bruh_new.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <conio.h>
 
@@ -15,6 +15,16 @@
 using namespace std;
 
 
+void display_init()
+{
+    for (int i = 0; i < HIGH; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            display[i][j].coord(j, i);
+        }
+    }
+}
 void displayClearField()
 {
     for (int i = 0; i < HIGH; ++i)
@@ -31,7 +41,7 @@ void displayFill()
     {
         for (int j = 0; j < WIDTH; ++j)
         {
-            if (!display[i][j].into) display[i][j].icon = emptyChar;
+            if (display[i][j].into == nullptr) { display[i][j].icon = emptyChar; }
             else { display[i][j].icon = display[i][j].into->icon; }
         };
     }
@@ -49,10 +59,9 @@ void displayOut()
 }
 
 
-
-
 int main()
 {
+    display_init();
     Entity player;
     player.link(&display[5][5]);
     player.icon = '@';
@@ -70,34 +79,33 @@ int main()
 
     while (main_flag)
     {
-        // -----------STEP 1: input-----------
-        // keyboard reciver
-        keyboardPress = _getch();
-        switch (keyboardPress)
-        {
-        case 'w':
-            player.getCoord().y -= 1;
-            player.direct = 1;
-            break;
-        case 'a':
-            player.getCoord().x -= 1;
-            player.direct = 2;
-            break;
-        case 'd':
-            player.getCoord().x += 1;
-            player.direct = 3;
-            break;
-        case 's':
-            player.getCoord().y += 1;
-            player.direct = 4;
-            break;
-        case ' ':
-            break;
-        case 27:
-            main_flag = false;  //exit
-            break;
-        }
-
+            // -----------STEP 1: input-----------
+            // keyboard reciver
+            keyboardPress = _getch();
+            switch (keyboardPress)
+            {
+            case 'w':
+                player.is_move = true;
+                player.direct = 1;
+                break;
+            case 'a':
+                player.is_move = true;
+                player.direct = 2;
+                break;
+            case 'd':
+                player.is_move = true;
+                player.direct = 3;
+                break;
+            case 's':
+                player.is_move = true;
+                player.direct = 4;
+                break;
+            case ' ':
+                break;
+            case 27:
+                main_flag = false;  //exit
+                break;
+            }
         // environment motor
         // здесь будет логика всех объектов: 
         // исполнение каких то паттернов движения, появление, применение свойств итд
@@ -116,13 +124,14 @@ int main()
                 //проверка на то, есть ли что-то в этой точке (into=nulptr - false - пустота)
                 if (display[tempCoord.y][tempCoord.x].into)
                 {
-                    display[tempCoord.y][tempCoord.x].into->collision_hanlder(curObj);
-                    curObj->collision_hanlder(display[tempCoord.y][tempCoord.x].into);
+                     display[tempCoord.y][tempCoord.x].into->collision_hanlder(curObj);
+                     curObj->collision_hanlder(display[tempCoord.y][tempCoord.x].into);
                 }
                 else
                 {
                     curObj->link(&display[tempCoord.y][tempCoord.x]);
                 }
+                curObj->is_move = false;
             }
         }
 
