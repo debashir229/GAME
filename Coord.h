f3bf88301bcbf31b8b6a966f2e974ff69d92a7e6
N <121445609+debashir229@users.noreply.github.com>
2024-02-01 19:18:33 +0400

    Coord.h

--- Coord.h
+++ Coord.h
@@ -5,9 +5,10 @@ using namespace std;
 class Coord
 {
 public:
-    int x = 0;
-    int y = 0;
-    Coord(int xP, int yP) : x{ xP }, y{ yP } {};
+    int x{ 0 };
+    int y{ 0 };
+    Coord(int xP, int yP) :
+        x{ xP }, y{ yP } {};
     Coord& operator()(int xP, int yP)
     {
         x = xP; y = yP;
